eb6ba86fb6bd0da6008d1c307d1665bcb41cac51
hackedpassword <wilydev@gmail.com>
2023-11-08 00:45:41 -0600

    Getting somewhere now! Best look out!

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,11 +33,25 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
+
+    // LibGDX Core
+    implementation("com.badlogicgames.gdx:gdx:1.12.0")
+
+    // LibGDX Backend for Android
+    implementation("com.badlogicgames.gdx:gdx-backend-android:1.12.0")
+
+    // LibGDX Native dependencies for Android
+    implementation("com.badlogicgames.gdx:gdx-platform:1.12.0:natives-armeabi")
+    implementation("com.badlogicgames.gdx:gdx-platform:1.12.0:natives-armeabi-v7a")
+    implementation("com.badlogicgames.gdx:gdx-platform:1.12.0:natives-arm64-v8a")
+    implementation("com.badlogicgames.gdx:gdx-platform:1.12.0:natives-x86")
+    implementation("com.badlogicgames.gdx:gdx-platform:1.12.0:natives-x86_64")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
