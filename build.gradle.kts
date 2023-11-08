f6427a105a512c8de768bf092c3132a3ed18adbf
hackedpassword <wilydev@gmail.com>
2023-11-08 12:00:05 -0600

    More whimsical than a millennial in squarepants!

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,5 +53,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("org.eclipse.jgit:org.eclipse.jgit:5.13.0.202109080827-r")
+
 }
 
